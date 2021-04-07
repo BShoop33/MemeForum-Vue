a4c86202cb9b7674a4f7e2dbe0f914178bda4ce1
Braxton Shoop <braxton.shoop@gmail.com>
2021-04-06 19:46:48 -0500

    saving to database completed

--- src/components/Meme.vue
+++ src/components/Meme.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>Hello</h1>
+  <div>
+    <div class="navbar_spacer"></div>
+    <v-img :src="imageURL" :width="800">
+      <h1 id="top">{{ top.toUpperCase() }}</h1>
+      <h1 id="bottom">{{ bottom.toUpperCase() }}</h1>
+    </v-img>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["top", "bottom", "imageURL"],
+};
 </script>
 
 <style scoped>
